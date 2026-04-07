0694340cb5f32fbbaa86964c0de7341580527611
melicordoba26 <melicordoba20@gmail.com>
2026-04-07 17:33:23 -0300

    nueva info

---
+++ MODULO2/CLASE10/ejercicio03.js
@@ -0,0 +1,19 @@
+let edad =17
+let nombre ="Melisa"
+if (edad > 18 || nombre =="Melisa"){
+    console.log(1)
+}
+else {
+    console.log(2)
+}
+
+if (edad<18 && nombre =="Melisa"){
+    console.log("a")
+}
+else if (edad >18){
+    console.log("b")
+    
+}
+else {
+    console.log("c")
+}
