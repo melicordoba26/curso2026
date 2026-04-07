9f9902d64208979ea5ca4f7a50e54e39e360adcc
melicordoba26 <melicordoba20@gmail.com>
2026-04-07 17:16:39 -0300

    condicionales

---
+++ MODULO2/CLASE10/condicionales.js
@@ -0,0 +1,18 @@
+// IF ELSE ELSEIF
+ let edad =7
+ let esMayor=edad >=18
+ if (esMayor){
+    console.log ("Es mayor de edad")
+ } else {
+    console.log("Es menor de edad")
+ }
+
+ let nombre="Melisa"
+ if (nombre=="Melisa"){
+    console.log("Hola Melisa")
+ } else if (edad>50){
+    console.log ("Es mayor de 50")
+   }  else {
+        console.log ("No permitido")
+    }
+ 
--- MODULO2/CLASE10/operadores.js
+++ MODULO2/CLASE10/operadores.js
@@ -10,6 +10,7 @@
  //operadores de comparacion
  let esIgual =3 ==3 //true
  let esIgual2 = 2 =='3' // ATMBIEN da true
+ let esDesigual =3 !== 'hola'//esto da true porque es desigual
  let esIgualEstrictamente = 3 === 3 // Esto da true
  let esIgualEstrictamente2 = 3 =='3' // Esto da false porque compara el tipo de dato y el valor
 
@@ -18,3 +19,22 @@
  let esMayorIgual = 3 > 3 // da True
  let esMenor = 3<2 // da false
  let esMenorIgual = 3<=2 //da false
+
+
+ //operadores logicos (logica de boole)
+ //AND OR NOT (Y O NO)
+ //AND DEVUELVE TRUE CUANDO LAS DOS EXPRESIONES QUE COMPARAMOS SON TRUE
+ true && true // true
+ true && false // false
+ false && true //false
+ false && false //false
+
+ //OR -->DEVUELVE TRUE CUANDO ALGUNA DE LAS DOS EXPRESIONES ES TRUE
+ true && true // true
+ true && false // true
+ false && true //true
+ false && false //false
+
+ //NOT--> INVIERTE EL ESTADO DE VERACIDAD DE ALGO
+ true //false
+ false//true
