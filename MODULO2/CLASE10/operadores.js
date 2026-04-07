520ff5d8610067d2606ac93215385fa8ef4b7592
melicordoba26 <melicordoba20@gmail.com>
2026-04-07 17:02:03 -0300

    operadores

---
+++ MODULO2/CLASE10/operadores.js
@@ -0,0 +1,20 @@
+// Operadores matemáticos
+ let suma = 3+5 // da 8
+ let resta= 5-3 // da 2
+ let mutltiplicacion= 5*3 //da 15
+ let division= 9/3 // da 3
+ let modulo  = 10 % 3 // Da 1, es el resto de la division entera. Se usa por ejemplo
+ let esPar = 5 % 2 ==0
+
+
+ //operadores de comparacion
+ let esIgual =3 ==3 //true
+ let esIgual2 = 2 =='3' // ATMBIEN da true
+ let esIgualEstrictamente = 3 === 3 // Esto da true
+ let esIgualEstrictamente2 = 3 =='3' // Esto da false porque compara el tipo de dato y el valor
+
+
+ let esMayor = 3 > 2 //da True
+ let esMayorIgual = 3 > 3 // da True
+ let esMenor = 3<2 // da false
+ let esMenorIgual = 3<=2 //da false
