7dbf89eb4259b16ae90c8e72f68b1c19b61e8f15
melicordoba26 <melicordoba20@gmail.com>
2026-04-13 07:15:22 -0300

    actividad

--- MODULO2/CLASE11/forloops.js
+++ MODULO2/CLASE11/forloops.js
@@ -2,3 +2,16 @@ let repeticiones = 10
 for (let i= 1; i<repeticiones; i++) {
     console.log (i)
 }
+
+
+for (let i= 10; i>=1; i--) {
+    console.log (i)
+}
+
+
+console.log("mostrar pares del 1 al 10")
+for (let i= 1; i<=10; i++){
+    if (i % 2 ==0)
+        console.log (i)
+}
+    
