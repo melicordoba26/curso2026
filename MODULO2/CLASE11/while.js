29751700c1d05183c79fdf88404dd3354607de9f
melicordoba26 <melicordoba20@gmail.com>
2026-04-14 06:35:38 -0300

    .

---
+++ MODULO2/CLASE11/while.js
@@ -0,0 +1,14 @@
+let i=0
+while (i<5){
+    console.log(i)
+    i ++
+}
+
+
+console.log("Ejemplo real")
+let password="1234"
+let intento =""
+while (intento !== password){
+   //* intento= prompt ("Ingrese su contraseña")
+}
+ //funciona solo en HTML
