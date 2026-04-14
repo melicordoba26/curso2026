348cc1b3f082d2b3cf3416dc50917b055dee862e
melicordoba26 <melicordoba20@gmail.com>
2026-04-14 06:58:18 -0300

    ejerciciosjs1

--- MODULO2/CLASE11/ejercicios.js
+++ MODULO2/CLASE11/ejercicios.js
@@ -4,3 +4,22 @@ for (let i=1; i<=10; i++){
     console.log(i)
 }
     
+console.log ("********************************************")
+
+console.log("Mostrar numeros pares del 1 al 20")
+for (let i=1; i<=20; i++){
+    if (i % 2 ==0)
+        console.log(i)
+}
+
+console.log ("********************************************")
+console.log("Mostrar tabla del 5")
+for (let i=1; i<=10; i++) {
+    console.log(i*5)
+}
+
+console.log ("********************************************")
+console.log("Contar hacia atras del 10 al 1")
+for (let i= 10; i>=1; i--){
+    console.log(i)
+}
